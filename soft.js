8dc6a5997b2d671595b6f51289ba7bf8c6016585
Kyn <63372753+markyndev@users.noreply.github.com>
2020-07-26 22:06:20 -0300

    Add files via upload

---
+++ soft.js
@@ -0,0 +1,28 @@
+
+// BASICO
+   var escrever = function (string){
+   console.log(string)
+   } 
+    var e_igual = function (um, dois){
+  if(um === dois){
+	  console.log("Verdadeiro")
+  } else{
+	   console.log("Falso")
+  }
+   } 
+ 
+// FUNÇÕES MATEMATICAS
+   var calcular = function (calculo){
+    console.log(calculo)
+}
+	   var randomico = function(){
+    return Math.floor(Math.random()*(999-0+1)+0);
+}
+	   var randomico_entre = function (inicio,final){
+    return Math.floor(Math.random()*(final-inicio+1)+inicio);
+}
+// CONSTANTES MATEMATICAS
+
+var constante_pi = 3.14159265359
+
+se_igual(1, 2)
